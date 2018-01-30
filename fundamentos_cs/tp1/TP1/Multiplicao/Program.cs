e99a205b569d5f041f445458069d02e8aa4e7c6e
magnoo <magno.oliveira@infnet.edu.br>
2018-01-30 19:45:12 -0200

    TP1 em uma solution
    Única solution com os 4 projetos solicitados no TP.

---
+++ fundamentos_cs/tp1/TP1/Multiplicao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicao
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Essa aplicação calcula a multiplicação de dois números!");
+
+            Console.WriteLine("Digite o primeiro número");
+            var n1 = Console.ReadLine();
+            Console.WriteLine("Digite o segundo número");
+            var n2 = Console.ReadLine();
+
+            int num1Val = Int32.Parse(n1);
+            int num2Val = Int32.Parse(n2);
+
+            Console.Write($"O resultado da multiplicação dos números {n1} e {n2} é: ");
+            Console.WriteLine(num1Val * num2Val);
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar.");
+            Console.ReadKey();
+        }
+    }
+}
