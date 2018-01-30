7a030cf08d81337155121953c649f9397ade666e
magnoo <magno.oliveira@infnet.edu.br>
2018-01-30 13:57:09 -0200

    TP1 Fundamentos C#

---
+++ fundamentos_cs/tp1/Divisao/Divisao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Essa aplicação calcula a divisão de dois números!");
+
+            Console.WriteLine("Digite o primeiro número");
+            var n1 = Console.ReadLine();
+            Console.WriteLine("Digite o segundo número");
+            var n2 = Console.ReadLine();
+
+            int num1Val = Int32.Parse(n1);
+            int num2Val = Int32.Parse(n2);
+
+            Console.Write($"O resultado da divisão dos números {n1} e {n2} é: ");
+            Console.WriteLine(num1Val / num2Val);
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar.");
+            Console.ReadKey();
+        }
+    }
+}
