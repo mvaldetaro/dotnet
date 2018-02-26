4fcfba6bc6fbb7f68792a89a58d0d761e3a402d6
magnoo <magno.oliveira@infnet.edu.br>
2018-02-26 18:06:40 -0300

    TP2 e TP3 .net

---
+++ web_asp_net/tp2-web-asp-net/calculadora/Controllers/SubtractionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace calculadora.Controllers
+{
+    public class SubtractionController : Controller
+    {
+        // GET: Subtraction
+        public int Index(string num1, string num2)
+        {
+            int num1Val = Int32.Parse(num1);
+            int num2Val = Int32.Parse(num2);
+            int result = num1Val - num2Val;
+
+            return result;
+        }
+    }
+}
---
+++ web_asp_net/tp2-web-asp-net/calculadora/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="calculadora.MvcApplication" Language="C#" %>
